9734912d2748e0c00b37687cd537be9ca15d8dba
tal rodgold <talrodgold@gmail.com>
2021-10-28 19:49:00 +0300

    edited hashtbl

--- targil_2/HashVolunteer.h
+++ targil_2/HashVolunteer.h
@@ -6,6 +6,16 @@
 using namespace std;
 class HashVolunteer:public HashTbls<Volunteer,string>
 {
-	
+public:
+	int H1(string key) override;
+	int H2(string key) override;
 };
 
+int HashVolunteer::H1(string key)
+{
+	return (stoi(key) % size);
+}
+inline int HashVolunteer::H2(string key)
+{
+	return 1 + (stoi(key) % (size - 1));
+}
